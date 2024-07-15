bae248e25d9e5a6885ab14af727a36c90209accc
Angelica Liguori <angelicaliguori@gmail.com>
2024-07-15 17:21:35 +0200

    added tensorboard path

--- SPG/main.py
+++ SPG/main.py
@@ -33,6 +33,9 @@ def main(fname):
     if not os.path.exists(params["SAVE_FOLDER"]):
         os.mkdir(params["SAVE_FOLDER"])
 
+    if not os.path.exists(os.path.join(params["SAVE_FOLDER"], 'logs')):
+        os.mkdir(os.path.join(params["SAVE_FOLDER"], 'logs'))
+
     if not os.path.exists(os.path.join(params["SAVE_FOLDER"], 'models', 'seeds')):
         os.mkdir(os.path.join(params["SAVE_FOLDER"], 'models', 'seeds'))
 
@@ -94,8 +97,8 @@ def main(fname):
         exp = f'RUN_{i}_SEED_{seed}_DATA_{dataset_name}_M_{embed_dim_m}_H_{num_heads_m}_L_{num_layers_m}_G_{embed_dim_g}_H_{num_heads_g}_L_{num_layers_g}_SPLIT_M_{perc_split_m}_G_T_{split_train}_V_{split_val}_reduction_{is_reduction}_combined_{is_combined}_max_cut_{max_cut}_max_cut_perc_{max_cut_perc}'
 
         file_path = f'{params["SAVE_FOLDER"]}/results/seeds/log_{exp}.txt'
-        tensorboard_path_m = f'{params["SAVE_FOLDER"]}/results/seeds/tb_M_{exp}.txt'
-        tensorboard_path_g = f'{params["SAVE_FOLDER"]}/results/seeds/tb_G_{exp}.txt'
+        tensorboard_path_m = f'{params["SAVE_FOLDER"]}/logs/tb_M_{exp}'
+        tensorboard_path_g = f'{params["SAVE_FOLDER"]}/logs/tb_G_{exp}'
 
         params['tensorboard_path_m'] = tensorboard_path_m
         params['tensorboard_path_g'] = tensorboard_path_g
